d71b3c878984b6d49a91d115c9a0e82678935a67
turky <160696449+Tiiilod@users.noreply.github.com>
2025-11-13 11:14:20 +0700

    update slicing-ui-4

---
+++ Slicing UI/Slicing-UI-4/slicing_ui_4/lib/halaman/promo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Promo extends StatelessWidget {
+  const Promo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Promo"),);
+  }
+}
